2524cff2d1208d74f344e461ff091e4e3be5326a
archit0 <architdwivedi00@gmail.com>
2026-01-21 17:56:46 +0530

    Bump to v1.0.4

--- Formula/termi.rb
+++ Formula/termi.rb
@@ -3,35 +3,35 @@ class Termi < Formula
 
   desc "Natural language terminal command executor using LLM APIs"
   homepage "https://github.com/archit0/homebrew-termi"
-  url "https://github.com/archit0/homebrew-termi/archive/refs/tags/v1.0.3.tar.gz"
-  sha256 "b11dfb3dee0b5d266285f2f9609ce506bed2313071334e65aa5b21e026a1a8ef"
+  url "https://github.com/archit0/homebrew-termi/archive/refs/tags/v1.0.4.tar.gz"
+  sha256 "bef76f5685aab16e22388418f661cb7fc3e0a5df1b5c431566b6d3dd788a444e"
   license "MIT"
 
   depends_on "python@3.12"
 
   resource "requests" do
     url "https://files.pythonhosted.org/packages/c9/74/b3ff8e6c8446842c3f5c837e9c3dfcfe2018ea6ecef224c710c85ef728f4/requests-2.32.5.tar.gz"
-    sha256 "dbba0bac56e100853db0ea71b82b4dfd5fe2bf6d3754a8893c3af500cec7d7cf"
+    sha256 "bef76f5685aab16e22388418f661cb7fc3e0a5df1b5c431566b6d3dd788a444e"
   end
 
   resource "charset-normalizer" do
     url "https://files.pythonhosted.org/packages/13/69/33ddede1939fdd074bce5434295f38fae7136463422fe4fd3e0e89b98062/charset_normalizer-3.4.4.tar.gz"
-    sha256 "94537985111c35f28720e43603b8e7b43a6ecfb2ce1d3058bbe955b73404e21a"
+    sha256 "bef76f5685aab16e22388418f661cb7fc3e0a5df1b5c431566b6d3dd788a444e"
   end
 
   resource "idna" do
     url "https://files.pythonhosted.org/packages/6f/6d/0703ccc57f3a7233505399edb88de3cbd678da106337b9fcde432b65ed60/idna-3.11.tar.gz"
-    sha256 "795dafcc9c04ed0c1fb032c2aa73654d8e8c5023a7df64a53f39190ada629902"
+    sha256 "bef76f5685aab16e22388418f661cb7fc3e0a5df1b5c431566b6d3dd788a444e"
   end
 
   resource "urllib3" do
     url "https://files.pythonhosted.org/packages/c7/24/5f1b3bdffd70275f6661c76461e25f024d5a38a46f04aaca912426a2b1d3/urllib3-2.6.3.tar.gz"
-    sha256 "1b62b6884944a57dbe321509ab94fd4d3b307075e0c2eae991ac71ee15ad38ed"
+    sha256 "bef76f5685aab16e22388418f661cb7fc3e0a5df1b5c431566b6d3dd788a444e"
   end
 
   resource "certifi" do
     url "https://files.pythonhosted.org/packages/e0/2d/a891ca51311197f6ad14a7ef42e2399f36cf2f9bd44752b3dc4eab60fdc5/certifi-2026.1.4.tar.gz"
-    sha256 "ac726dd470482006e014ad384921ed6438c457018f4b3d204aea4281258b2120"
+    sha256 "bef76f5685aab16e22388418f661cb7fc3e0a5df1b5c431566b6d3dd788a444e"
   end
 
   def install
