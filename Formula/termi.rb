ea0534a97bd534bccd85c4d50a420381028c0929
archit0 <architdwivedi00@gmail.com>
2026-01-21 18:33:12 +0530

    Fix corrupted SHA256 values in formula

--- Formula/termi.rb
+++ Formula/termi.rb
@@ -11,27 +11,27 @@ class Termi < Formula
 
   resource "requests" do
     url "https://files.pythonhosted.org/packages/c9/74/b3ff8e6c8446842c3f5c837e9c3dfcfe2018ea6ecef224c710c85ef728f4/requests-2.32.5.tar.gz"
-    sha256 "a67769a4669beee1f47bc785ef1e2c21ea8ae3d3d79bc2d1c217f83eb54268ea"
+    sha256 "dbba0bac56e100853db0ea71b82b4dfd5fe2bf6d3754a8893c3af500cec7d7cf"
   end
 
   resource "charset-normalizer" do
     url "https://files.pythonhosted.org/packages/13/69/33ddede1939fdd074bce5434295f38fae7136463422fe4fd3e0e89b98062/charset_normalizer-3.4.4.tar.gz"
-    sha256 "a67769a4669beee1f47bc785ef1e2c21ea8ae3d3d79bc2d1c217f83eb54268ea"
+    sha256 "94537985111c35f28720e43603b8e7b43a6ecfb2ce1d3058bbe955b73404e21a"
   end
 
   resource "idna" do
     url "https://files.pythonhosted.org/packages/6f/6d/0703ccc57f3a7233505399edb88de3cbd678da106337b9fcde432b65ed60/idna-3.11.tar.gz"
-    sha256 "a67769a4669beee1f47bc785ef1e2c21ea8ae3d3d79bc2d1c217f83eb54268ea"
+    sha256 "795dafcc9c04ed0c1fb032c2aa73654d8e8c5023a7df64a53f39190ada629902"
   end
 
   resource "urllib3" do
     url "https://files.pythonhosted.org/packages/c7/24/5f1b3bdffd70275f6661c76461e25f024d5a38a46f04aaca912426a2b1d3/urllib3-2.6.3.tar.gz"
-    sha256 "a67769a4669beee1f47bc785ef1e2c21ea8ae3d3d79bc2d1c217f83eb54268ea"
+    sha256 "1b62b6884944a57dbe321509ab94fd4d3b307075e0c2eae991ac71ee15ad38ed"
   end
 
   resource "certifi" do
     url "https://files.pythonhosted.org/packages/e0/2d/a891ca51311197f6ad14a7ef42e2399f36cf2f9bd44752b3dc4eab60fdc5/certifi-2026.1.4.tar.gz"
-    sha256 "a67769a4669beee1f47bc785ef1e2c21ea8ae3d3d79bc2d1c217f83eb54268ea"
+    sha256 "ac726dd470482006e014ad384921ed6438c457018f4b3d204aea4281258b2120"
   end
 
   def install
